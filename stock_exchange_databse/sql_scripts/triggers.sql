8ee6a503c14bf1c805b6c079780b5171b8d61a05
naseef944 <54270245+naseef944@users.noreply.github.com>
2024-10-02 14:25:08 +0200

    Update triggers.sql

--- stock_exchange_databse/sql_scripts/triggers.sql
+++ stock_exchange_databse/sql_scripts/triggers.sql
@@ -1,1 +1,15 @@
+DELIMITER //
+
+CREATE TRIGGER before_stock_update 
+BEFORE UPDATE ON stocks_list
+FOR EACH ROW
+BEGIN
+    -- Log the old price in the price history
+    IF (OLD.current_price != NEW.current_price) THEN
+        INSERT INTO stock_price_history (stock_ID, date, old_price)
+        VALUES (OLD.ID, CURDATE(), OLD.current_price);
+    END IF;
+END //
+
+DELIMITER ;
 
