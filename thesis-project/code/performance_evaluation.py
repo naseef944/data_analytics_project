db8da5722de23134fd1bd4d530dbe36d20d7422a
naseef944 <54270245+naseef944@users.noreply.github.com>
2024-10-10 14:50:16 +0200

    Update performance_evaluation.py

--- thesis-project/code/performance_evaluation.py
+++ thesis-project/code/performance_evaluation.py
@@ -1,1 +1,12 @@
-
+from sklearn.metrics import precision_score, recall_score, f1_score, classification_report
+precision = precision_score(y_test, tr_pred_rf_, average='weighted')
+print("Random forest Precision:", precision)
+# Recall
+recall = recall_score(y_test, tr_pred_rf_, average='weighted')
+print("Random forest Recall:", recall)
+# F1-score
+f1_score = f1_score(y_test, tr_pred_rf_, average='weighted')
+print("Random forest F1-Score:", f1_score)
+#classification_report
+print("Classification Report:")
+print(classification_report(y_test, tr_pred_rf_))
